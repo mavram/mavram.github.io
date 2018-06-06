7d236f7a602b9c7152301caad70552bb9ae96424
mavram <mavram@gmail.com>
2018-06-06 18:54:23 -0400

    removed javascript code

--- index.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(() => {
-    $('#about-title-as-resume').hide()
-    $('#about-title-as-page').click(() => {
-        $('#about-title-as-resume').show()
-        $('#about-title-as-page').hide()
-    })
-})
